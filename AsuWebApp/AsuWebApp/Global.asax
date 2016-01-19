a4eab75426a4484f2b6360c6b28817c27b72b9e3
Aleh_Skamarokhau <s_oleg94@mail.ru>
2016-01-19 23:20:22 +0200

    AsuWebApp: Init commit.

---
+++ AsuWebApp/AsuWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AsuWebApp.MvcApplication" Language="C#" %>
---
+++ AsuWebApp/AsuWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AsuWebApp.Startup))]
+namespace AsuWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
