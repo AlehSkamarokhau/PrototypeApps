e9058d54b89f20f6bc02223c1f92337c4f7923f1
Aleh Skamarokhau <aleh_skamarokhau@epam.com>
2016-02-28 13:56:51 +0300

    ModelingLogicalSchemes: Добавил интерфейс для взаимодействия с чёрным ящиком.

---
+++ ModelingLogicalSchemes/ModelingLogicalSchemes.Components/MagicalBlackBoxes/Interfaces/IBlackBox.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+using ModelingLogicalSchemes.Components.Entities;
+
+namespace ModelingLogicalSchemes.Components.MagicalBlackBoxes.Interfaces
+{
+	/// <summary>
+	/// Интерфейс взаимодействия с чёрным ящиком.
+	/// </summary>
+	public interface IBlackBox
+	{
+		/// <summary>
+		/// Sets the configuration.
+		/// </summary>
+		/// <param name="configuration">The configuration.</param>
+		void SetConfiguration(IDictionary<int, BrokenTypes> configuration);
+
+		/// <summary>
+		/// Gets the output value.
+		/// </summary>
+		/// <param name="inputValues">The input values.</param>
+		/// <returns>The output values.</returns>
+		bool[] GetOutputValue(params bool[] inputValues);
+	}
+}
