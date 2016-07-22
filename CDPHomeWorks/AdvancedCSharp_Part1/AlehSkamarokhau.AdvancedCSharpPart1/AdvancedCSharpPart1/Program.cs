5d09dba0a7f0243918d1e65f6901841c54ce84a6
Aleh Skamarokhau <aleh_skamarokhau@epam.com>
2016-07-22 15:15:09 +0300

    CDPHomeWorks: Implement "Advanced C# (Types) 1"

---
+++ CDPHomeWorks/AdvancedCSharp_Part1/AlehSkamarokhau.AdvancedCSharpPart1/AdvancedCSharpPart1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace AdvancedCSharpPart1
+{
+	struct InfoData
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+	}
+
+	sealed class Source
+	{
+		internal void CheckAndProceed(List<InfoData> data)
+		{
+			var dest = new Destination();
+
+			//do something
+
+			dest.ProceedData<InfoData>(data);
+		}
+	}
+
+	sealed class Destination
+	{
+		internal void ProceedData<TData>(IList<TData> data)
+		{
+			for (int i = 0; i < data.Count; i++)
+			{
+				//do something
+			}
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
