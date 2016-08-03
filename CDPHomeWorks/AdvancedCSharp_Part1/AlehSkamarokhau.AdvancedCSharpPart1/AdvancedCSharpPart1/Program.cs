2fb43a447308d7132e4acd65a0ccf34edd0a40ad
Aleh Skamarokhau <aleh_skamarokhau@epam.com>
2016-08-03 15:57:16 +0300

    CDPHomeWorks: "Advanced C# (Types) 1" Add performance counters.

--- CDPHomeWorks/AdvancedCSharp_Part1/AlehSkamarokhau.AdvancedCSharpPart1/AdvancedCSharpPart1/Program.cs
+++ CDPHomeWorks/AdvancedCSharp_Part1/AlehSkamarokhau.AdvancedCSharpPart1/AdvancedCSharpPart1/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace AdvancedCSharpPart1
 {
@@ -12,21 +14,46 @@ namespace AdvancedCSharpPart1
 	{
 		internal void CheckAndProceed(List<InfoData> data)
 		{
-			var dest = new Destination();
+			var dest = new Destination<InfoData>(new List<InfoData>());
 
 			//do something
 
-			dest.ProceedData<InfoData>(data);
+			dest.ProceedDataFor(data);
 		}
 	}
 
-	sealed class Destination
+	sealed class Destination<TData>
 	{
-		internal void ProceedData<TData>(IList<TData> data)
+		private IList<TData> _items;
+
+		public Destination(IList<TData> items)
+		{
+			_items = items;
+		}
+
+		public IList<TData> Items
+		{
+			get { return _items; }
+			set { _items = value; }
+		}
+
+		internal void ProceedDataFor(IList<TData> data)
 		{
 			for (int i = 0; i < data.Count; i++)
 			{
 				//do something
+
+				_items.Add(data[i]);
+			}
+		}
+
+		internal void ProceedDataForeach(IList<TData> data)
+		{
+			foreach (var currentItem in data)
+			{
+				//do something
+
+				_items.Add(currentItem);
 			}
 		}
 	}
@@ -35,6 +62,35 @@ namespace AdvancedCSharpPart1
 	{
 		static void Main(string[] args)
 		{
+			int countObj = 10000000;
+
+			List<InfoData> items = new List<InfoData>(countObj);
+
+			for (int i = 0; i < items.Capacity; i++)
+			{
+				items.Add(new InfoData() { FirstName = i.ToString(), LastName = i.ToString() });
+			}
+
+			Destination<InfoData> dest = new Destination<InfoData>(new List<InfoData>(countObj));
+
+			Stopwatch sw = new Stopwatch();
+
+			sw.Start();
+			dest.ProceedDataFor(items);
+			sw.Stop();
+
+			Console.WriteLine(String.Format("With FOR loop: {0}", sw.ElapsedMilliseconds));
+
+			sw.Reset();
+			dest.Items.Clear();
+
+			sw.Start();
+			dest.ProceedDataForeach(items);
+			sw.Stop();
+
+			Console.WriteLine(String.Format("With FOREACH loop: {0}", sw.ElapsedMilliseconds));
+
+			Console.ReadLine();
 		}
 	}
 }
