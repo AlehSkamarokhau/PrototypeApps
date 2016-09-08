f88eaca8e0bc8940c9038010f7493dda606fddad
Aleh_Skamarokhau <s_oleg94@mail.ru>
2016-09-09 00:05:46 +0300

    CDPHomeWorks: LINQ. Review.

--- CDPHomeWorks/LINQ/AlehSkamarokhau.LINQ/Task/LinqSamples.cs
+++ CDPHomeWorks/LINQ/AlehSkamarokhau.LINQ/Task/LinqSamples.cs
@@ -5,17 +5,10 @@
 //Copyright (C) Microsoft Corporation.  All rights reserved.
 
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Windows.Forms;
-using System.Xml.Linq;
 using SampleSupport;
-using Task;
 using Task.Data;
-
-// Version Mad01
 
 namespace SampleQueries
 {
@@ -23,46 +16,10 @@ namespace SampleQueries
 	[Prefix("Linq")]
 	public class LinqSamples : SampleHarness
 	{
-		private const int DEFAULT_SIZE_SPLIT_LINE = 70;
-
 		private DataSource dataSource = new DataSource();
 
-		[Category("Restriction Operators")]
-		[Title("Example 1. Where - Task 1")]
-		[Description("This sample uses the where clause to find all elements of an array with a value less than 5.")]
-		public void LinqExample1()
-		{
-			int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
-
-			var lowNums =
-				from num in numbers
-				where num < 5
-				select num;
-
-			Console.WriteLine("Numbers < 5:");
-			foreach (var x in lowNums)
-			{
-				Console.WriteLine(x);
-			}
-		}
-
-		[Category("Restriction Operators")]
-		[Title("Example 2. Where - Task 2")]
-		[Description("This sample return return all presented in market products")]
-		public void LinqExample2()
-		{
-			var products =
-				from p in dataSource.Products
-				where p.UnitsInStock > 0
-				select p;
-
-			foreach (var p in products)
-			{
-				ObjectDumper.Write(p);
-			}
-		}
-
 		[Title("Task 1")]
+		[Description("Выдайте список всех клиентов, чей суммарный оборот (сумма всех заказов) превосходит некоторую величину X. Продемонстрируйте выполнение запроса с различными X (подумайте, можно ли обойтись без копирования запроса несколько раз).")]
 		public void Linq1()
 		{
 			var inputValues = new decimal[] { 5000M, 10000M, 20000M };
@@ -85,7 +42,6 @@ namespace SampleQueries
 
 				foreach (var currentCustomer in results[currentKey])
 				{
-					//TODO: Impruve. Don't get Sum Orders again.
 					Console.WriteLine("Customer ID = {0} Company Name = {1} Sum Orders = {2}", currentCustomer.CustomerID, currentCustomer.CompanyName, currentCustomer.Orders.Sum(o => o.Total));
 				}
 
@@ -94,10 +50,9 @@ namespace SampleQueries
 		}
 
 		[Title("Task 2")]
+		[Description("Для каждого клиента составьте список поставщиков, находящихся в той же стране и том же городе. Сделайте задания с использованием группировки и без.")]
 		public void Linq2()
 		{
-			//TODO: Check results.
-
 			Dictionary<Customer, IList<Supplier>> customersAndSupplies = new Dictionary<Customer, IList<Supplier>>();
 			Dictionary<Customer, IList<IGrouping<string, Supplier>>> customersAndSuppliesGrouped = new Dictionary<Customer, IList<IGrouping<string, Supplier>>>();
 
@@ -112,34 +67,26 @@ namespace SampleQueries
 
 			Console.WriteLine("Result without grouping by City:\r\n");
 
-			ConsoleHelper.WriteSplitLine('=', DEFAULT_SIZE_SPLIT_LINE);
-
 			foreach (var currentCustomer in customersAndSupplies.Keys)
 			{
 				if (customersAndSupplies[currentCustomer].Count > 0)
 				{
-					Console.WriteLine("Customer - {0}, Country - {1}, City - {2}:\r\n", currentCustomer.CompanyName, currentCustomer.Country, currentCustomer.City);
+					Console.WriteLine("Customer - {0}, Country - {1}, City - {2}:", currentCustomer.CompanyName, currentCustomer.Country, currentCustomer.City);
 
 					foreach (var currentSupplier in customersAndSupplies[currentCustomer])
 					{
 						Console.WriteLine("{0} :: {1}, {2}", currentSupplier.SupplierName, currentSupplier.Country, currentSupplier.City);
 					}
-
-					Console.WriteLine("\r\n");
 				}
 			}
 
-			ConsoleHelper.WriteSplitLine('=', DEFAULT_SIZE_SPLIT_LINE);
-
-			Console.WriteLine("Result with grouping by City:\r\n");
-
-			ConsoleHelper.WriteSplitLine('=', DEFAULT_SIZE_SPLIT_LINE);
+			Console.WriteLine("\r\nResult with grouping by City:\r\n");
 
 			foreach (var currentCustomer in customersAndSuppliesGrouped.Keys)
 			{
 				if (customersAndSuppliesGrouped[currentCustomer].Count > 0)
 				{
-					Console.WriteLine("Customer - {0}, Country - {1}, City - {2}:\r\n", currentCustomer.CompanyName, currentCustomer.Country, currentCustomer.City);
+					Console.WriteLine("Customer - {0}, Country - {1}, City - {2}:", currentCustomer.CompanyName, currentCustomer.Country, currentCustomer.City);
 
 					foreach (var currentGroupSupplies in customersAndSuppliesGrouped[currentCustomer])
 					{
@@ -148,15 +95,12 @@ namespace SampleQueries
 							Console.WriteLine("{0} :: {1}, {2}", currentSupplier.SupplierName, currentSupplier.Country, currentSupplier.City);
 						}
 					}
-
-					Console.WriteLine("\r\n");
 				}
 			}
-
-			ConsoleHelper.WriteSplitLine('=', DEFAULT_SIZE_SPLIT_LINE);
 		}
 
 		[Title("Task 3")]
+		[Description("Найдите всех клиентов, у которых были заказы, превосходящие по сумме величину X.")]
 		public void Linq3()
 		{
 			var price = 3000M;
@@ -169,8 +113,6 @@ namespace SampleQueries
 
 				Console.WriteLine("Customer ID - {0}, Company Name - {1}, Count Orders - {2}:", currentCustomer.CustomerID, currentCustomer.CompanyName, findCustomerOrders.Count());
 
-				ConsoleHelper.WriteSplitLine('=', DEFAULT_SIZE_SPLIT_LINE);
-
 				foreach (var currentOrder in findCustomerOrders)
 				{
 					Console.WriteLine("Order ID - {0}, Total - {1}", currentOrder.OrderID, currentOrder.Total);
@@ -181,6 +123,7 @@ namespace SampleQueries
 		}
 
 		[Title("Task 4")]
+		[Description("Выдайте список клиентов с указанием, начиная с какого месяца какого года они стали клиентами (принять за таковые месяц и год самого первого заказа).")]
 		public void Linq4()
 		{
 			var customers = dataSource.Customers.Where(c => c.Orders.Count() > 0).Select(c => new
@@ -201,6 +144,7 @@ namespace SampleQueries
 		}
 
 		[Title("Task 5")]
+		[Description("Сделайте предыдущее задание, но выдайте список отсортированным по году, месяцу, оборотам клиента (от максимального к минимальному) и имени клиента.")]
 		public void Linq5()
 		{
 			var customers = dataSource.Customers.Where(c => c.Orders.Count() > 0).Select(c => new
@@ -225,6 +169,7 @@ namespace SampleQueries
 		}
 
 		[Title("Task 6")]
+		[Description("Укажите всех клиентов, у которых указан нецифровой код или не заполнен регион или в телефоне не указан код оператора (для простоты считаем, что это равнозначно «нет круглых скобочек в начале»).")]
 		public void Linq6()
 		{
 			var customers = dataSource.Customers.Where(new Func<Customer, bool>((Customer c) =>
@@ -242,6 +187,7 @@ namespace SampleQueries
 		}
 
 		[Title("Task 7")]
+		[Description("Сгруппируйте все продукты по категориям, внутри – по наличию на складе, внутри последней группы отсортируйте по стоимости.")]
 		public void Linq7()
 		{
 			var products = dataSource.Products.GroupBy(prd => prd.Category)
@@ -256,6 +202,7 @@ namespace SampleQueries
 		}
 
 		[Title("Task 8")]
+		[Description("Сгруппируйте товары по группам «дешевые», «средняя цена», «дорогие». Границы каждой группы задайте сами.")]
 		public void Linq8()
 		{
 			var highPrice = 100M;
@@ -288,6 +235,7 @@ namespace SampleQueries
 		}
 
 		[Title("Task 9")]
+		[Description("Рассчитайте среднюю прибыльность каждого города (среднюю сумму заказа по всем клиентам из данного города) и среднюю интенсивность (среднее количество заказов, приходящееся на клиента из каждого города).")]
 		public void Linq9()
 		{
 			var averageCityProfitability = dataSource.Customers.GroupBy(cst => cst.City)
@@ -314,6 +262,7 @@ namespace SampleQueries
 		}
 
 		[Title("Task 10")]
+		[Description("Сделайте среднегодовую статистику активности клиентов по месяцам (без учета года), статистику по годам, по годам и месяцам (т.е. когда один месяц в разные годы имеет своё значение).")]
 		public void Linq10()
 		{
 			var monthStatistics = dataSource.Customers.Select(cst => new { CustomerID = cst.CustomerID,
