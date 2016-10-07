5e442da79df058cdaf3b4db69a2ca9b5686ac9d4
Aleh_Skamarokhau <s_oleg94@mail.ru>
2016-10-07 17:08:19 +0300

    CDPHomeWorks: SQL Part 1. Subtask 1.2 (SSDT). Configure solution.

---
+++ CDPHomeWorks/SQL_Part1/SSDT/Northwind/Northwind_v110/dbo/Tables/CreditCards.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[CreditCards] (
+    [CreditCardID]   INT           IDENTITY (1, 1) NOT NULL,
+    [CardNumber]     NVARCHAR (20) NOT NULL,
+    [ExpirationDate] DATETIME      NOT NULL,
+    [CardHolderName] NVARCHAR (31) NOT NULL,
+    [EmployeeID]     INT           NOT NULL,
+    CONSTRAINT [PK_CreditCards] PRIMARY KEY CLUSTERED ([CreditCardID] ASC),
+    CONSTRAINT [FK_CreditCards_Employees] FOREIGN KEY ([EmployeeID]) REFERENCES [dbo].[Employees] ([EmployeeID])
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [CardHolderName]
+    ON [dbo].[CreditCards]([CardHolderName] ASC);
+
