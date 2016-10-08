ae97f6521672fd705843e179d020b146cbb82587
Aleh_Skamarokhau <s_oleg94@mail.ru>
2016-10-08 08:24:35 +0300

    CDPHomeWorks: SQL Part 1. Implemented tasks #2.1.3; #2.2.1; #2.2.2; #2.2.3

--- CDPHomeWorks/SQL_Part1/TasksForGetData.sql
+++ CDPHomeWorks/SQL_Part1/TasksForGetData.sql
@@ -3,7 +3,7 @@ GO
 
 -- ******************** TASK # 2.1.1 ************************
 
---SELECT OrderID, ShippedDate, ShipVia FROM Orders WHERE ShippedDate > '1998-05-06' AND ShipVia >= 2;
+--SELECT OrderID, ShippedDate, ShipVia FROM Orders WHERE ShippedDate >= '1998-05-06' AND ShipVia >= 2;
 
 -- HELPERS QUERIES
 
@@ -18,3 +18,24 @@ GO
 
 --SELECT * FROM Orders WHERE ShippedDate IS NULL;
 --SELECT COUNT(*) FROM Orders;
+
+-- ******************** TASK # 2.1.3 ************************
+
+--SELECT "Order ID" = OrderID, 
+--	   "Shipped Date" = CASE WHEN ShippedDate IS NULL THEN 'Not Shipped' END
+--FROM Orders WHERE ShippedDate > '1998-05-06' OR ShippedDate IS NULL;
+
+-- ******************** TASK # 2.2.1 ************************
+
+--SELECT ContactName, Country FROM Customers WHERE Country IN ('USA', 'Canada') ORDER BY ContactName, Country;
+
+-- ******************** TASK # 2.2.2 ************************
+
+--SELECT ContactName, Country FROM Customers WHERE Country NOT IN ('USA', 'Canada') ORDER BY ContactName;
+
+-- ******************** TASK # 2.2.3 ************************
+
+--SELECT DISTINCT Country FROM Customers ORDER BY Country DESC;
+
+
+
