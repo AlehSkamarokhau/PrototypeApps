0e4502acfcbeeca30fdc203f4aab84dafa3828f6
Aleh_Skamarokhau <s_oleg94@mail.ru>
2016-10-08 11:22:21 +0300

    CDPHomeWorks: SQL Part 1. Implemented tasks #2.3.1; #2.3.2; #2.3.3; #2.4.1

--- CDPHomeWorks/SQL_Part1/TasksForGetData.sql
+++ CDPHomeWorks/SQL_Part1/TasksForGetData.sql
@@ -3,7 +3,7 @@ GO
 
 -- ******************** TASK # 2.1.1 ************************
 
---SELECT OrderID, ShippedDate, ShipVia FROM Orders WHERE ShippedDate >= '1998-05-06' AND ShipVia >= 2;
+SELECT OrderID, ShippedDate, ShipVia FROM Orders WHERE ShippedDate >= '1998-05-06' AND ShipVia >= 2;
 
 -- HELPERS QUERIES
 
@@ -12,7 +12,7 @@ GO
 
 -- ******************** TASK # 2.1.2 ************************
 
---SELECT OrderId, "ShippedDate" = CASE WHEN ShippedDate IS NULL THEN 'Not Shipped' END FROM Orders WHERE ShippedDate IS NULL;
+SELECT OrderId, "ShippedDate" = CASE WHEN ShippedDate IS NULL THEN 'Not Shipped' END FROM Orders WHERE ShippedDate IS NULL;
 
 -- HELPERS QUERIES
 
@@ -21,21 +21,34 @@ GO
 
 -- ******************** TASK # 2.1.3 ************************
 
---SELECT "Order ID" = OrderID, 
---	   "Shipped Date" = CASE WHEN ShippedDate IS NULL THEN 'Not Shipped' END
---FROM Orders WHERE ShippedDate > '1998-05-06' OR ShippedDate IS NULL;
+SELECT "Order ID" = OrderID, 
+	   "Shipped Date" = CASE WHEN ShippedDate IS NULL THEN 'Not Shipped' END
+FROM Orders WHERE ShippedDate > '1998-05-06' OR ShippedDate IS NULL;
 
 -- ******************** TASK # 2.2.1 ************************
 
---SELECT ContactName, Country FROM Customers WHERE Country IN ('USA', 'Canada') ORDER BY ContactName, Country;
+SELECT ContactName, Country FROM Customers WHERE Country IN ('USA', 'Canada') ORDER BY ContactName, Country;
 
 -- ******************** TASK # 2.2.2 ************************
 
---SELECT ContactName, Country FROM Customers WHERE Country NOT IN ('USA', 'Canada') ORDER BY ContactName;
+SELECT ContactName, Country FROM Customers WHERE Country NOT IN ('USA', 'Canada') ORDER BY ContactName;
 
 -- ******************** TASK # 2.2.3 ************************
 
---SELECT DISTINCT Country FROM Customers ORDER BY Country DESC;
+SELECT DISTINCT Country FROM Customers ORDER BY Country DESC;
 
+-- ******************** TASK # 2.3.1 ************************
 
+SELECT DISTINCT OrderID FROM [Order Details] WHERE Quantity BETWEEN 3 AND 10;
 
+-- ******************** TASK # 2.3.2 ************************
+
+SELECT CustomerID, Country FROM Customers WHERE Country BETWEEN 'B' AND 'H' ORDER BY Country;
+
+-- ******************** TASK # 2.3.3 ************************
+
+SELECT CustomerID, Country FROM Customers WHERE SUBSTRING(Country, 1, 1) IN ('B', 'C', 'D', 'E', 'F', 'G') ORDER BY Country;
+
+-- ******************** TASK # 2.4.1 ************************
+
+SELECT ProductID, ProductName FROM Products WHERE LOWER(ProductName) LIKE '%cho_olade%';
